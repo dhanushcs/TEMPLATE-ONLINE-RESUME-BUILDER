c34dbb3edcdb36f2b0b1be40c985c4375eae0865
dhanushcs <csdhanush5@gmail.com>
2022-10-05 14:03:43 +0530

    Allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 #aadded manually
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
